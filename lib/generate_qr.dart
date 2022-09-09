a92cf18f1da619b34b4b1122072aea9959d4d530
Your Name <Your Email>
2022-09-09 16:32:05 +0530

    QR Code generator added

---
+++ lib/generate_qr.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+class GenerateScreen extends StatefulWidget {
+  const GenerateScreen({Key? key}) : super(key: key);
+
+  @override
+  State<GenerateScreen> createState() => _GenerateScreenState();
+}
+
+class _GenerateScreenState extends State<GenerateScreen> {
+  final TextEditingController _textController = TextEditingController();
+  String dummyData = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: Container(
+        padding: const EdgeInsets.fromLTRB(20, 50, 20, 0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    'QR Code Generator',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              const Divider(),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: const [
+                  Text(
+                    'Enter your details',
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: _textController,
+                decoration: const InputDecoration(
+                  hintText: 'Type anything',
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                ),
+                onPressed: () {
+                  setState(
+                    () {
+                      dummyData = (_textController.text == ''
+                          ? null
+                          : _textController.text)!;
+                    },
+                  );
+                },
+                child: const Text(
+                  'Submit',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              QrImage(
+                data: dummyData,
+                size: 250,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
